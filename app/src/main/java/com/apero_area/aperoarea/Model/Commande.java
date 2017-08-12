a9841639a4448d0a5502d8ea0dc01c79e5b1f6f5
dany <daniel.sautot@gmail.com>
2017-08-12 19:25:26 +0200

    1.2.5 btn minus et add

--- app/src/main/java/com/apero_area/aperoarea/Model/Commande.java
+++ app/src/main/java/com/apero_area/aperoarea/Model/Commande.java
@@ -1,9 +1,25 @@
 package com.apero_area.aperoarea.Model;
 
+import java.util.List;
+
 /**
  * Created by dany on 12-Aug-17.
  */
 
 public class Commande {
-    
+
+
+    private List<Product> cart;
+
+
+
+    public List<Product> getCart() {
+        return cart;
+    }
+
+    public void setCart(List<Product> cart) {
+        this.cart = cart;
+    }
+
+
 }
--- app/src/main/java/com/apero_area/aperoarea/RecyclerAdapter.java
+++ app/src/main/java/com/apero_area/aperoarea/RecyclerAdapter.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -42,11 +44,11 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
         holder.name.setText(products.get(position).getName());
         holder.short_description.setText(products.get(position).getShort_description());
         holder.price.setText(products.get(position).getPrice());
-
+final int test = position;
         holder.bind(products.get(position), listener);
 
 
@@ -54,6 +56,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
                 .load(products.get(position).getImages().get(0).getSrc())
                 .error(R.mipmap.ic_launcher)
                 .into(holder.image);
+
+        holder.addproduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              // Log.d("test", products.get(test).getName());
+                Log.d("test", "click");
+            }
+        });
     }
 
     @Override
@@ -66,6 +76,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
         TextView name, short_description, price;
         ImageView image;
+        ImageButton addproduct, minusproduct;
 
         public MyViewHolder(View itemView) {
             super(itemView);
@@ -73,6 +84,9 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
             short_description = (TextView)itemView.findViewById(R.id.short_description);
             price = (TextView)itemView.findViewById(R.id.price);
             image = (ImageView)itemView.findViewById(R.id.imageProduct);
+            addproduct = (ImageButton) itemView.findViewById(R.id.addproduct);
+            minusproduct = (ImageButton) itemView.findViewById(R.id.minusproduct);
+
         }
 
         public void bind(final Product products, final OnItemClickListener listener) {
@@ -82,6 +96,10 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
                 }
             });
         }
+
+
+
+
     }
 
 }
