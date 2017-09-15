01796a10f3c775f56bb8db16ac9808cd5420ad8e
dany <daniel.sautot@gmail.com>
2017-09-15 16:08:33 +0200

    1.6.8 zone de livraison définie, périmètre effectué, possibilité de livrer dans la zone, si pas dans la zone on peut quand meme poursuivre la navigation

--- app/src/main/java/com/apero_area/aperoarea/view/activities/MapsActivity.java
+++ app/src/main/java/com/apero_area/aperoarea/view/activities/MapsActivity.java
@@ -1,14 +1,29 @@
 package com.apero_area.aperoarea.view.activities;
 
 import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
 import android.location.Location;
+
+import com.apero_area.aperoarea.domain.helper.RequestUserPermission;
 import com.google.android.gms.location.LocationListener;
+
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.apero_area.aperoarea.R;
@@ -21,9 +36,14 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.PolyUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by stran on 11/09/2017.
@@ -39,6 +59,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Location mLastLocation;
     Marker mCurrLocationMarker;
     LocationRequest mLocationRequest;
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +89,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            RequestUserPermission requestUserPermission = new
+                    RequestUserPermission(MapsActivity.this);
+            requestUserPermission.verifyPositionPermissions();
+            //requestUserPermission.forceFullOpenPermission();
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        mMap.setMyLocationEnabled(true);
+        if (location != null)
+        {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(location.getLatitude(), location.getLongitude()), 14));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(14)                   // Sets the zoom
+                    .bearing(0)                // Sets the orientation of the camera to north
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+
+
         //Initialize Google Play Services
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this,
@@ -75,11 +122,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     == PackageManager.PERMISSION_GRANTED) {
                 buildGoogleApiClient();
                 mMap.setMyLocationEnabled(true);
+                progress = ProgressDialog.show(this, "Chargement des données",
+                        "En cours de chargement, veuillez patienter", true);
             }
         }
         else {
             buildGoogleApiClient();
             mMap.setMyLocationEnabled(true);
+            progress = ProgressDialog.show(this, "Chargement des données",
+                    "En cours de chargement, veuillez patienter", true);
         }
     }
 
@@ -90,6 +141,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .addApi(LocationServices.API)
                 .build();
         mGoogleApiClient.connect();
+
     }
 
     @Override
@@ -103,6 +155,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
         }
 
     }
@@ -117,15 +170,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mLastLocation = location;
         if (mCurrLocationMarker != null) {
-            mCurrLocationMarker.remove();
+            mCurrLocationMarker.remove(); //show the old location then of the old /= than the current location, do the code bellow
         }
+        progress.dismiss();
 
         //Place current location marker
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
         markerOptions.title("Votre position");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
         mCurrLocationMarker = mMap.addMarker(markerOptions);
 
         //move map camera
@@ -136,6 +190,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (mGoogleApiClient != null) {
             LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
         }
+        shippingArea(mLastLocation);
 
     }
 /*
@@ -215,7 +270,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 } else {
 
                     // Permission denied, Disable the functionality that depends on this permission.
-                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "permission refusée", Toast.LENGTH_LONG).show();
                 }
                 return;
             }
@@ -224,4 +279,84 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // You can add here other case statements according to your requirement.
         }
     }
+    public void shippingArea (Location location) {
+
+        mLastLocation = location;
+
+
+            //Polygon polygon = Polygon.Builder() with only .addVertex and build() at the end
+            List<LatLng> area = new ArrayList<>();
+            area.add(new LatLng(48.879179, 2.278419));//200m outside the Palais des Congrès
+            area.add(new LatLng(48.886697, 2.283214));//200m outside Porte de Champerret
+            area.add(new LatLng(48.899486, 2.306624));//200m outside Porte de Clichy
+            area.add(new LatLng(48.904734, 2.344236));//200m outside Porte de Clignancourt
+            area.add(new LatLng(48.904466, 2.393116));//200m outside Porte de la Villette
+            area.add(new LatLng(48.896420, 2.419133));//Premier point extrémité de Pantin (proche de BETC)
+            area.add(new LatLng(48.887362, 2.423654));//Deuxième point extrémité de Pantin (proche de Collège Marie Curie)
+            area.add(new LatLng(48.879142, 2.412828));//200m outside Porte des Lilas
+            area.add(new LatLng(48.846136, 2.421416));//200m outside Porte de Vincennes
+            area.add(new LatLng(48.827322, 2.405320));//200m outside Porte de Charenton
+            area.add(new LatLng(48.812033, 2.361685));//200m outside Porte d'Italie
+            area.add(new LatLng(48.832119, 2.253060));//200m outside Porte de Saint Cloud
+            area.add(new LatLng(48.872508, 2.268112));//200m outside Porte de Dauphine
+
+
+            // Get back the mutable Polygon
+            //Polygon polygon = mMap.addPolygon(rectOptions);
+            //boolean shipping = polygon.contains(point);
+
+        if(location!= null) {
+
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            boolean shipping = PolyUtil.containsLocation(latLng, area, true);
+
+            if (shipping) {
+                Intent intent = new Intent(getApplicationContext(),
+                MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(getBaseContext(), "Vous êtes bien dans la zone de livraison", Toast.LENGTH_LONG).show();
+                Log.i("Test", "location" + latLng.toString());
+
+
+            } else {
+                SupportMapFragment map = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+                View v = getSupportFragmentManager().findFragmentById(R.id.map).getView();
+                v.setAlpha(0.2f);
+
+                TextView question = (TextView) findViewById(R.id.question);
+                question.setVisibility(View.VISIBLE);
+
+                Button oui = (Button) findViewById(R.id.checkBoxOui);
+                oui.setVisibility(View.VISIBLE);
+                oui.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getBaseContext(), "Vous ne pourrez pas commander de produits", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+                Button non = (Button) findViewById(R.id.checkBoxNon);
+                non.setVisibility(View.VISIBLE);
+                non.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                        System.exit(0);
+                    }
+                });
+
+            }
+        }
+
+
+    }
+
+    public GoogleMap getMap(){
+        return mMap;
+    }
+
+
 }
