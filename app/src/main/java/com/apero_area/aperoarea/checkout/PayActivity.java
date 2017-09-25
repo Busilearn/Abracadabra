74c2b791a96f4a330e162a69d9a116903eda17e3
micado94 <micado94@msn.com>
2017-09-25 19:22:06 +0200

    1.8.1 transfert

--- app/src/main/java/com/apero_area/aperoarea/checkout/PayActivity.java
+++ app/src/main/java/com/apero_area/aperoarea/checkout/PayActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.apero_area.aperoarea.R;
 import com.apero_area.aperoarea.model.CenterRepository;
@@ -17,6 +19,8 @@ import com.stripe.android.model.Token;
 import com.stripe.android.view.CardInputWidget;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PayActivity extends AppCompatActivity {
     private Communicator communicator;
@@ -28,10 +32,13 @@ public class PayActivity extends AppCompatActivity {
     private TextView cardMail;
     private TextView cardPhoneNumber;
     private TextView notes;
+    private Button submitButton;
     private ArrayList<String> idProduct = new ArrayList<String>();
     private ArrayList<String> quantityProduct = new ArrayList<String>();
     Location loc = null;
     String posGps = null;
+    CardInputWidget mCardInputWidget = null;
+    Boolean test = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,19 +50,81 @@ public class PayActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         amount = extras.getString("plan_price");
         loc = (Location) getIntent().getParcelableExtra("loc");
-        posGps = loc.getLatitude()+ " , " + loc.getLongitude();
+        //TODO Stocker la position gps
+        posGps = loc.getLatitude() + " , " + loc.getLongitude();
 
-        stripe = new Stripe(getApplicationContext(),"pk_test_QxNQHsJ0MaEiKDtVAlcj7Qk0");
-    }
+        stripe = new Stripe(getApplicationContext(), "pk_test_QxNQHsJ0MaEiKDtVAlcj7Qk0");
 
-    public void submitCard(View view) {
+        mCardInputWidget = (CardInputWidget) findViewById(R.id.card_input_widget);
+
         cardName = (TextView) findViewById(R.id.cardName);
         cardFirstName = (TextView) findViewById(R.id.cardFirstName);
         cardMail = (TextView) findViewById(R.id.cardMail);
         cardPhoneNumber = (TextView) findViewById(R.id.cardPhoneNumber);
         notes = (TextView) findViewById(R.id.notes);
+        submitButton = (Button) findViewById(R.id.submitButton);
 
-        CardInputWidget mCardInputWidget = (CardInputWidget) findViewById(R.id.card_input_widget);
+        submitButton.setClickable(false);
+
+
+        cardName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    if (cardName.getText().length() < 3) {
+                        cardName.setError("Merci d'entrer au minimum 3 caractères");
+                        submitButton.setClickable(false);
+                    }
+                    else
+                        submitButton.setClickable(true);
+                }
+            }
+        });
+
+        cardFirstName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    if (cardFirstName.getText().length() < 3) {
+                        cardFirstName.setError("Merci d'entrer au minimum 3 caractères");
+                        submitButton.setClickable(false);
+                    }
+                    else
+                        submitButton.setClickable(true);
+                }
+            }
+        });
+
+        cardMail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    if (!(isEmailValid(cardMail.getText().toString()))) {
+                        cardMail.setError("Merci d'entrer une adresse mail valide");
+                        submitButton.setClickable(false);
+                    }
+                    else
+                        submitButton.setClickable(true);
+                }
+            }
+        });
+
+        cardPhoneNumber.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    if (cardPhoneNumber.getText().length() < 10) {
+                        cardPhoneNumber.setError("Merci d'entrer un numéro de téléphone valide");
+                        submitButton.setClickable(false);
+                    }
+                    else
+                        submitButton.setClickable(true);
+                }
+            }
+        });
+    }
+
+    public void submitCard(View view) {
 
         Card cardToSave = mCardInputWidget.getCard();
         if (cardToSave != null) {
@@ -81,11 +150,30 @@ public class PayActivity extends AppCompatActivity {
             });
         }
         else {
-            Log.e("test", "Invalid Card Data");
-            //mErrorDialogHandler.showError("Invalid Card Data");
+            Toast.makeText(getBaseContext(), "Le numéro de carte entré n'est pas valide", Toast.LENGTH_LONG).show();
         }
     }
 
+    public boolean isEmailValid(String email)
+    {
+        String regExpn =
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                        +"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        +"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                        +"([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(regExpn,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+
+        if(matcher.matches())
+            return true;
+        else
+            return false;
+    }
 }
 
 
